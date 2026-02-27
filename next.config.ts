2bbee165a5c8b5a020277c62e0a54eb768293b4e
Abd Elrahman Essam Mohamed <bodyessam97a@gmail.com>
2026-02-27 12:12:26 +0200

    Enhance Next.js configuration with i18n support and update dependencies. Refactor global styles and layout for improved design consistency. Replace default content in the homepage with a button component.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+/** @type {import('next-i18next').UserConfig} */
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "ar"],
+  },
+  images: {
+      // domains: ["images.unsplash.com", "images.pexels.com"],
+  },
 };
 
 export default nextConfig;
